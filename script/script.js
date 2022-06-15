154e72ac51e222937479c4bfd719b1cdd0d28673
Ekaterina Podchalimova <pod4alimova.ekaterina@yandex.ru>
2022-06-15 13:29:17 +0300

    style:изменен стиль карточек с событиями

--- script/script.js
+++ script/script.js
@@ -19,16 +19,31 @@ const cards = [
 
 function openPopup(evt) {
     evt.classList.add('popup_opened');
+    document.addEventListener('keydown', keyHandler);
 }
 
 function closePopup(evt) {
     evt.classList.remove('popup_opened');
+    document.removeEventListener('keydown', keyHandler);
 }
 
 function openInstruction(element) {
     openPopup(popupTypeInstruction);
     popupCard.src = element.src;
 }
+
+function keyHandler(evt) {
+    const openedPopup = document.querySelector('.popup_opened');
+    if (evt.key === "Escape") {
+        closePopup(openedPopup);
+    }
+}
+
+document.addEventListener('click', function (evt) {
+    if (evt.target.classList.contains('popup_opened')){
+        closePopup(evt.target);
+    }
+});
 
 cards.forEach(function (element) {
         const instructionElement = templateElement.querySelector('.instruction__image').cloneNode(true);
